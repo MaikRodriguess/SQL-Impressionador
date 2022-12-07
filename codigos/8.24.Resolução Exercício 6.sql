6e689805c1a745736cc2f701355117313ca13f90
Maik Rodrigues <69226200+MaikRodriguess@users.noreply.github.com>
2022-12-07 17:25:40 -0300

    Com comentario

--- codigos/8.24.Resolução Exercício 6.sql
+++ codigos/8.24.Resolução Exercício 6.sql
@@ -1,4 +1,14 @@
-SELECT * FROM DimScenario
+/*
+6. Vamos continuar analisando a tabela FactStrategyPlan. Além da coluna AccountKey que 
+identifica o tipo de conta, há também uma outra coluna chamada ScenarioKey. Essa coluna 
+possui a numeração que identifica o tipo de cenário: Real, Orçado e Previsão.
+Faça um INNER JOIN para criar uma tabela contendo o ScenarioName para cada ScenarioKey
+da tabela FactStrategyPlan. O seu SELECT final deve conter as colunas:
+• StrategyPlanKey
+• DateKey
+• ScenarioName 
+• Amount
+*/
 
 SELECT 
 	StrategyPlanKey,
