1df7b6781fe46402b9fcf68fda47147dbc7af192
Maik Rodrigues <69226200+MaikRodriguess@users.noreply.github.com>
2023-05-15 16:58:41 -0300

    Update 10.14.PRINT - Printando uma mensagem na tela.sql

--- codigos/10.14.PRINT - Printando uma mensagem na tela.sql
+++ codigos/10.14.PRINT - Printando uma mensagem na tela.sql
@@ -1,13 +1,13 @@
 -- Utilizando o PRINT para mostrar o resultado de uma consulta
 
-SET NOCOUNT ON -- Não mostra quantidade de linhas afetadas em mensagens 
+SET NOCOUNT ON -- NÃ£o mostra quantidade de linhas afetadas em mensagens 
 
 -- Exempo 1: Printe na tela a quantidade de lojas On e a quantidade de lojas off da tabela DimStore. 
--- Utilize variavéis para isso.
+-- Utilize variavÃ©is para isso.
 DECLARE @varLojasOn INT, @varLojasOff INT
 SET @varLojasOn = (SELECT COUNT(*) FROM DimStore WHERE Status = 'on')
 SET @varLojasOff = (SELECT COUNT(*) FROM DimStore WHERE Status = 'off')
-SELECT @varLojasOff AS 'Lojas Abertas', @varLojasOff AS 'Lojas Abertas'
+SELECT @varLojasOn AS 'Lojas Abertas', @varLojasOff AS 'Lojas fechadas'
 
-PRINT 'O total de lojas abertas é de: ' + CAST(@varLojasOff AS VARCHAR(30))
-PRINT 'O total de lojas fechadas é de: ' + CAST(@varLojasOn AS VARCHAR(30))
+PRINT 'O total de lojas abertas Ã© de: ' + CAST(@varLojasOn AS VARCHAR(30))
+PRINT 'O total de lojas fechadas Ã© de: ' + CAST(@varLojasOff AS VARCHAR(30))
