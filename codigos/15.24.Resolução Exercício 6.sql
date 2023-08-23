23d23802b5900a568a4da06d134a4925b6edccbc
Maik Rodrigues <maikrs007@gmail.com>
2023-08-23 11:14:05 -0300

    ready

--- codigos/15.24.Resolução Exercício 6.sql
+++ codigos/15.24.Resolução Exercício 6.sql
@@ -1,3 +1,18 @@
 /* 6. A empresa implementou um programa de fidelização de clientes empresariais. 
-Todos aqueles que comprarem mais de 3000 unidades de um mesmo produto receberá descontos em outras compras.
+Todos aqueles que comprarem mais de 3000 unidades de um mesmo produto receberá descontos
+em outras compras.
+
+Você deverá descobrir as informações de CustomerKey e CampanyName destes clientes
 */
+
+SELECT 
+	CustomerKey,
+	CompanyName
+FROM DimCustomer
+WHERE CustomerKey IN (
+			SELECT
+				CustomerKey
+			FROM FactOnlineSales
+			GROUP BY CustomerKey, ProductKey
+			HAVING COUNT(*) >= 3000
+			)
